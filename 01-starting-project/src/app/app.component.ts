57377de327d02c8a08b483032523f9ebdc62a2c2
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-04 12:36:48 -0500

    Refactor user component to remove signals and use direct input properties for user data

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({ // comes from the Angular framework (being imported).
   selector: 'app-root', // tells Angular what elements it should replace with this components markup.
@@ -11,4 +12,6 @@ import { UserComponent } from "./user/user.component";
 })
 export class AppComponent { // creates a custom HTML element; not just a class, contains a decorator of information that adds metadata to this class.
 
+  // how to expose the data of DUMMY_USERS to this component so we can use it in the users template.
+  users = DUMMY_USERS;
 }
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-// Signals; special values that tell Angular when they CHANGE.
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,18 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // signal for this; a contain that contains a value and when you change the value, Angular will be notified and will identify all the places where the value is being used, and will update the value in those places.
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  
+ @Input({required: true}) avatar!: string;
+ @Input({required: true}) name!: string;
+ @Input({required: true}) id!: string;
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
-
-  onSelectUser() { // draws a new random user everytime the button is clicked. We can also now CALL this function in the html file.
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // updated the signal here:
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
