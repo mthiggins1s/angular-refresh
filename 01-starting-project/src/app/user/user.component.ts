433184c4825b01b5435c157c0a23f00d1bbbafe1
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-04 12:19:55 -0500

    Refactor user component to use signals for selected user and image path

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+// Signals; special values that tell Angular when they CHANGE.
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,5 +12,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // signal for this; a contain that contains a value and when you change the value, Angular will be notified and will identify all the places where the value is being used, and will update the value in those places.
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
+
+  onSelectUser() { // draws a new random user everytime the button is clicked. We can also now CALL this function in the html file.
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // updated the signal here:
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
