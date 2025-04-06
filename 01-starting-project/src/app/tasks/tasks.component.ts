1542afdfafc5b0565fc892020bd9a831ebc6fac8
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-06 17:15:52 -0500

    Refactor task and user components to enhance task filtering and user selection; add user model interface.

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -11,5 +11,37 @@ import { TaskComponent } from "./task/task.component";
 export class TasksComponent {
   // this ? tells TS that we know that this might NOT be SET. TS doesnt know if this value is initialized or not.
     // adding the | symbol creates a UNION type; tells TS that the type of value that can be store will be either type string OR type undefined.
-  @Input() name: string | undefined;
+  @Input({required: true}) userId!: string
+  @Input({required: true}) name!: string;
+
+  // all tasks must be assigned to a user, so you must add a userId property to the object.
+  tasks = [
+    {
+      id: 't1',
+      userId: 'ul',
+      title: 'Master Angular',
+      summary: 'Learn all the basics of Angular',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  // .filter takes an arrow function. This condition will check if the userId is equal to this.id.
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,10 +1,5 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
- }
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
